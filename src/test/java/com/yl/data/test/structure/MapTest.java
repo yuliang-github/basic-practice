7cf7513328e00adb0148dc58ce7501735a1c9a5a
alex <824187670@qq.com>
2019-05-20 19:05:57 +0800

    map

---
+++ src/test/java/com/yl/data/test/structure/MapTest.java
@@ -0,0 +1,34 @@
+package com.yl.data.test.structure;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Alex
+ * @since 2019/5/20 14:04
+ */
+public class MapTest {
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> map = new HashMap<>();
+        map.put("2", 1);
+
+        String key = "2";
+        int  h = 0;
+        int hashcode = (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+
+        System.err.println(hashcode);
+
+        Map<String,Integer> cmap = new ConcurrentHashMap<>();
+
+    }
+
+    @Test
+    public void demo_1(){
+        System.err.println(0x7fffffff);
+    }
+}
